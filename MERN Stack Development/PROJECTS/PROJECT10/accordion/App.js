c72537d328b4905d838835e24ed6d04fa0c85a3f
MANOJ KUMAR <109135089+BCAPATHSHALA@users.noreply.github.com>
2022-12-09 16:37:42 +0530

    Add files via upload

---
+++ MERN Stack Development/PROJECTS/PROJECT10/accordion/App.js
@@ -0,0 +1,12 @@
+import Accordion from "./components/Accordion";
+import "./components/style.css";
+
+function App() {
+  return (
+    <div className="outer-box">
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
