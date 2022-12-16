10af37d5e196cec5910c5d428bfe919aec9bb909
MANOJ KUMAR <109135089+BCAPATHSHALA@users.noreply.github.com>
2022-12-16 11:21:34 +0530

    React JS

--- MERN Stack Development/27. REACT CLASS/Project Using Redux/redux/store.js
+++ MERN Stack Development/27. REACT CLASS/Project Using Redux/redux/store.js
@@ -1,12 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import messageSlice from "./slices/messageSlice";
-import notesSlice from "./slices/notesSlice";
 import grocessarySlice from "./slices/grocessrySlice";
 
 export default configureStore({
   reducer: {
-    messageReducer: messageSlice, // all reducers of messageSlice are defined in the reducer messageReducer
-    notesReducer: notesSlice,
     grocReducer: grocessarySlice,
   },
 });
