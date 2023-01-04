657fe7b6f5dbaf0f9d99fc07278bc57a0f8f0361
MANOJ KUMAR <109135089+BCAPATHSHALA@users.noreply.github.com>
2023-01-04 15:00:00 +0530

    Delete MERN Stack Development/server directory

--- MERN Stack Development/server/controllers/postsController.js
+++
@@ -1,8 +0,0 @@
-const getAllPostController = async (req, res) => {
-  console.log(req._id);
-  return res.send("These are all the post");
-};
-
-module.exports = {
-  getAllPostController,
-};
--- MERN Stack Development/server/routers/authRouter.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const authController = require("../controllers/authController");
-
-router.post("/signup", authController.signupController);
-router.post("/login", authController.loginController);
-router.post("/refresh", authController.refreshAccessTokenController);
-
-module.exports = router;
